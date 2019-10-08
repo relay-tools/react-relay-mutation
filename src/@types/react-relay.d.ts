f9fef1d9e8a0f3591c80003bd3fa728e0a4c150b
Jimmy Jia <tesrin@gmail.com>
2019-10-07 21:26:20 -0400

    chore: Remove unnecessary typings (#322)

--- src/@types/react-relay.d.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from 'react';
-import 'react-relay';
-import { CRelayContext, Environment } from 'relay-runtime';
-
-declare module 'react-relay' {
-  export const ReactRelayContext: Context<CRelayContext<Environment>>;
-}
